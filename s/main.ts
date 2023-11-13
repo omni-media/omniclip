8857838e03acad027a464ed6d454e7f33eb9acca
zenkyu <przemekgg2002@gmail.com>
2023-11-13 17:20:00 +0100

    delete: panels folder

--- s/main.ts
+++ s/main.ts
@@ -1,19 +1,17 @@
 import {register_to_dom} from "@benev/slate"
 import {ConstructEditor, single_panel_layout} from "@benev/construct/x/mini.js"
 
-import {AboutPanel} from "./panels/about/panel.js"
 import {omnislate, OmniContext} from "./context/slate.js"
 import {TimelinePanel} from "./components/omni-timeline/panel.js"
 import {OmniTimeline} from "./components/omni-timeline/component.js"
 
 omnislate.context = new OmniContext({
 	panels: {
-		AboutPanel,
 		TimelinePanel
 	},
 	layouts: {
-		empty: single_panel_layout("AboutPanel"),
-		default: single_panel_layout("AboutPanel"),
+		empty: single_panel_layout("TimelinePanel"),
+		default: single_panel_layout("TimelinePanel"),
 	},
 })
 
