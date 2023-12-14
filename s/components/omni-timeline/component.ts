e570a2fab2c94accb3a4c5e024e52411b7b18d35
zenkyu <przemekgg2002@gmail.com>
2023-12-14 22:05:22 +0100

    add: playhead to timeline component and refactor a bit

--- s/components/omni-timeline/component.ts
+++ s/components/omni-timeline/component.ts
@@ -4,6 +4,7 @@ import {styles} from "./styles.js"
 import {Clip} from "./views/clip/view.js"
 import {Track} from "./views/track/view.js"
 import {Toolbar} from "./views/toolbar/view.js"
+import {Playhead} from "./views/playhead/view.js"
 import {TimeRuler} from "./views/time-ruler/view.js"
 import {shadow_component} from "../../context/slate.js"
 import {Indicator} from "../../context/controllers/timeline/types.js"
@@ -15,6 +16,8 @@ export const OmniTimeline = shadow_component({styles}, use => {
 	use.watch(() => use.context.state.timeline)
 	const state = use.context.state.timeline
 	const dnd = use.context.controllers.timeline.drag
+	const playhead_drag = use.context.controllers.timeline.playhead_drag
+	const [coordinates, setCoordinates] = use.state<V2>([0, 0])
 
 	const augmented_dragover = (event: DragEvent) => {
 		const {clientX, clientY} = event
@@ -22,11 +25,13 @@ export const OmniTimeline = shadow_component({styles}, use => {
 		const indicator = (event.target as HTMLElement).part.value as Indicator
 		const timeline = use.shadow.querySelector(".timeline-relative")
 		const coordinates = coordinates_in_rect(pointerCoordinates, timeline!.getBoundingClientRect())
-
 		dnd.dropzone.dragover({
 			coordinates: coordinates!,
 			indicator: indicator,
 		})(event)
+		playhead_drag.dropzone.dragover(coordinates!)(event)
+		if(coordinates)
+			setCoordinates(coordinates)
 	}
 
 	const render_tracks = () => state.tracks.map((_track) => Track([], {attrs: {part: "add-track-indicator"}}))
@@ -42,6 +47,7 @@ export const OmniTimeline = shadow_component({styles}, use => {
 			<div
 				style="width: ${calculate_timeline_width(state.clips, state.zoom)}px"
 				class=timeline-relative>
+				${Playhead([coordinates])}
 				${render_tracks()}
 				${render_clips()}
 				${ProposalIndicator()}
