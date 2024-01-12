4c939c867de06caaa55c6e8c7d529d09179c6e96
Przemek Galezki <przemekgg2002@gmail.com>
2024-01-12 17:33:23 +0100

    refactor: compositor and tweak it to work with new text effect features

--- s/context/controllers/compositor/controller.ts
+++ s/context/controllers/compositor/controller.ts
@@ -1,5 +1,6 @@
 import {pub, reactor, signal} from "@benev/slate"
 
+import {TextManager} from "./parts/text-manager.js"
 import {VideoManager} from "./parts/video-manager.js"
 import {TimelineActions} from "../timeline/actions.js"
 import {AnyEffect, XTimeline} from "../timeline/types.js"
@@ -7,68 +8,46 @@ import {AnyEffect, XTimeline} from "../timeline/types.js"
 export class Compositor {
 
 	on_playing = pub()
-	
+
 	#is_playing = signal(false)
 	#last_time = 0
 	#pause_time = 0
-	#currently_played_effects: AnyEffect[] = []
+	currently_played_effects: AnyEffect[] = []
 
-	#worker = new Worker(new URL("./worker.js", import.meta.url), {type: "module"});
-	#actions: TimelineActions
 	readonly canvas = document.createElement("canvas")
-	
-	VideoManager = new VideoManager()
+	readonly ctx = this.canvas.getContext("2d")
 
-	constructor(actions: TimelineActions) {
-		this.VideoManager.add_video({
+	#VideoManager = new VideoManager(this)
+	TextManager: TextManager
+
+	constructor(private actions: TimelineActions) {
+		this.canvas.width = 1280
+		this.canvas.height = 720
+
+		this.TextManager = new TextManager(this, actions)
+		this.#VideoManager.add_video({
 			id: "252", kind: "video", src: "/public/bbb_video_avc_frag.mp4",
 			start: 0, end: 60000, track: 1, start_at_position: 5000, duration: 60000
 		})
-		this.#actions = actions
-		const offscreen = this.canvas.transferControlToOffscreen();
-		this.#worker.postMessage({
-			canvas: offscreen,
-			type: "canvas"
-		}, [offscreen])
-		this.#worker.addEventListener("message", (frame: MessageEvent<VideoFrame>) => {
-			frame.data.close()
-		})
+
 		reactor.reaction(
 			() => this.#is_playing.value,
 			(is_playing) => {
 				this.#pause_time = performance.now() - this.#last_time
 				this.#on_playing(is_playing)
 				if(is_playing) {
-					this.#play_videos()
-				} else this.#pause_videos()
+					this.#VideoManager.play_videos()
+				} else this.#VideoManager.pause_videos()
 			}
 		)
-	}
-	
-	#pause_videos() {
-		for(const effect of this.#currently_played_effects) {
-			if(effect.kind === "video") {
-				const video = this.VideoManager.get(effect.id)
-				video?.pause()
-			}
-		}
-	}
-
-	async #play_videos() {
-		for(const effect of this.#currently_played_effects) {
-			if(effect.kind === "video") {
-				const video = this.VideoManager.get(effect.id)
-				await video?.play()
-			}
-		}
 	}
 
 	#on_playing = (is_playing: boolean) => {
 		if(is_playing) {
 			const elapsed_time = this.#calculate_elapsed_time()
-			this.#actions.increase_timecode(elapsed_time)
+			this.actions.increase_timecode(elapsed_time)
 			this.on_playing.publish(0)
-			this.#draw_effect()
+			this.draw_effects()
 		}
 		requestAnimationFrame(() => this.#on_playing(this.#is_playing.value))
 	}
@@ -82,61 +61,67 @@ export class Compositor {
 
 	#sort_effects_by_track(effects: AnyEffect[]) {
 		// so that effects on first track draw on top of things that are on second track
-		const sorted_effects = effects.sort((a, b) => {
+		const sorted_effects = [...effects].sort((a, b) => {
 			if(a.track < b.track) return 1
 				else return -1
 		})
 		return sorted_effects
 	}
 
-	async #draw_effect() {
-		const effects_sorted_by_track = this.#sort_effects_by_track(this.#currently_played_effects!)
-		if(this.#currently_played_effects) {
+	async draw_effects(redraw?: boolean) {
+		if(redraw) {this.clear_canvas()}
+		const effects_sorted_by_track = this.#sort_effects_by_track(this.currently_played_effects!)
+		if(this.currently_played_effects) {
 			for(const effect of effects_sorted_by_track) {
 				if(effect.kind === "video") {
-					const video = this.VideoManager.get(effect.id)
-					if(video?.paused) {await video.play()}
+					const video = this.#VideoManager.get(effect.id)
+					if(!redraw && video?.paused) {await video.play()}
 					try {
 						const frame = new VideoFrame(video!)
-						this.#worker.postMessage({
-							frame,
-							type: "frame"
-						})
+						this.#VideoManager.draw_video_frame(frame)
 						frame.close()
 					} catch(e) {console.log(e)}
 				}
 				else if(effects_sorted_by_track.length <= 0) {
-					this.#worker.postMessage({
-						type: "clear-canvas"
-					})
+					this.clear_canvas()
 				}
 				else if(effect.kind === "text") {
-					this.#worker.postMessage({
-						item: effect,
-						type: "text"
-					})
+					this.TextManager.draw_text(effect)
 				}
 			}
 		}
 	}
 
-	get_effects_relative_to_timecode(state: XTimeline, timecode: number) {
+	clear_canvas() {
+		this.ctx?.clearRect(0, 0, 1280, 720)
+	}
+
+	get_effects_relative_to_timecode(state: XTimeline) {
+		const timecode = state.timecode
 		const effects = state.effects.filter(effect => effect.start_at_position <= timecode && effect.start_at_position + effect.duration >= timecode)
 		return effects.length > 0 ? effects : undefined
 	}
 
-	set_currently_played_effects(effects: AnyEffect[]) {
-		this.#currently_played_effects = effects
+	set_currently_played_effects(timeline: XTimeline) {
+		const effects_relative_to_timecode = this.get_effects_relative_to_timecode(timeline)
+		if(effects_relative_to_timecode)
+		this.currently_played_effects = effects_relative_to_timecode
+	}
+
+	update_currently_played_effect(effect: AnyEffect) {
+		const updated = this.currently_played_effects.map(e => e.id === effect.id ? e = effect : e) as AnyEffect[]
+		const sorted = this.#sort_effects_by_track(updated)
+		this.currently_played_effects = sorted
 	}
 
 	set_video_playing = (playing: boolean) => {
 		this.#is_playing.value = playing
-		this.#actions.set_is_playing(playing)
+		this.actions.set_is_playing(playing)
 	}
 
 	toggle_video_playing = () => {
 		this.#is_playing.value = !this.#is_playing.value
-		this.#actions.toggle_is_playing()
+		this.actions.toggle_is_playing()
 	}
 
 }
--- s/context/controllers/compositor/parts/video-manager.ts
+++ s/context/controllers/compositor/parts/video-manager.ts
@@ -1,6 +1,11 @@
+import {Compositor} from "../controller.js"
 import {VideoEffect} from "../../timeline/types.js"
 
 export class VideoManager extends Map<string, HTMLVideoElement> {
+
+	constructor(private compositor: Compositor) {
+		super()
+	}
 
 	add_video(effect: VideoEffect) {
 		const video = document.createElement('video');
@@ -14,6 +19,35 @@ export class VideoManager extends Map<string, HTMLVideoElement> {
 
 		this.set(effect.id, video)
 		// video.preload = 'auto';
+	}
+
+	draw_video_frame(frame: VideoFrame) {
+		this.compositor.ctx!.drawImage(
+			frame,
+			0,
+			0,
+			this.compositor.canvas.width,
+			this.compositor.canvas.height
+		)
+		frame.close()
+	}
+
+	pause_videos() {
+		for(const effect of this.compositor.currently_played_effects) {
+			if(effect.kind === "video") {
+				const video = this.get(effect.id)
+				video?.pause()
+			}
+		}
+	}
+
+	async play_videos() {
+		for(const effect of this.compositor.currently_played_effects) {
+			if(effect.kind === "video") {
+				const video = this.get(effect.id)
+				await video?.play()
+			}
+		}
 	}
 
 	// play(startTime: number, endTime: number): void {
