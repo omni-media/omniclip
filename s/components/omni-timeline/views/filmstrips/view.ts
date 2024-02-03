ad3673b0ea1195e8849476b96a7ee154ab99a22e
Przemek Galezki <przemekgg2002@gmail.com>
2024-02-03 01:21:26 +0100

    refactor: filmstrips

--- s/components/omni-timeline/views/filmstrips/view.ts
+++ s/components/omni-timeline/views/filmstrips/view.ts
@@ -9,38 +9,27 @@ import {calculate_start_position} from "../../utils/calculate_start_position.js"
 export const Filmstrips = shadow_view({styles}, use => (effect: VideoEffect, timeline: GoldElement) => {
 	use.watch(() => use.context.state.timeline.zoom)
 	const ffmpeg = use.context.helpers.ffmpeg
-	const [_thumbnails, setThumbnails, getThumbnails] = use.state<string[]>([])
-	const [visibleThumbnails, setVisibleThumbnails, getVisibleThumbnails] = use.state<string[]>([])
+	const [_thumbnails, setFilmstrips, getFilmstrips] = use.state<string[]>([])
+	const [visibleFilmstrips, setVisibleFilmstrips] = use.state<string[]>([])
 	const [startAt, setStartAt] = use.state(0)
 	const [_, setLastEffectOffsetLeftPosition, getLastEffectOffseLeftPosition] = use.state(0)
 
 	use.setup(() => {
 		timeline.addEventListener("scroll", () => {
-			const width_of_frame = calculate_effect_width(effect, 2) / getThumbnails().length
-			const margin = width_of_frame * 2
-			const effect_left = calculate_start_position(effect.start_at_position, use.context.state.timeline.zoom)
-			const normalized_left = Math.floor((timeline.scrollLeft - effect_left) / width_of_frame) * width_of_frame - margin < 0
-			? 0
-			: Math.floor((timeline.scrollLeft - effect_left) / width_of_frame) * width_of_frame - margin
-			const frame_count = timeline.clientWidth / width_of_frame
-			const difference = Math.abs(normalized_left - getLastEffectOffseLeftPosition())
-			if(difference >= Math.floor(width_of_frame)) {
-				const new_arr = []
-				setLastEffectOffsetLeftPosition(normalized_left)
-				for(let i = 0; i<= frame_count;i+=1) {
-					const effect_width = calculate_effect_width(effect, use.context.state.timeline.zoom)
-					const position = normalized_left + width_of_frame * i
-					if(position <= effect_width)
-						new_arr.push(getThumbnails()[get_filmstrip_at(position)])
-				}
-				setStartAt(normalized_left)
-				setVisibleThumbnails(new_arr)
+			const result = recalculate_all_visible_filmstrips()
+			if(result) {
+				setVisibleFilmstrips(result.visible)
+				setStartAt(result.normalized_left)
+				setLastEffectOffsetLeftPosition(result.normalized_left)
 			}
 		})
 
 		watch.track(() => use.context.state.timeline.zoom, (zoom) => {
-			const visible = recalculate_all_visible_images(getThumbnails())
-			setVisibleThumbnails(visible)
+			const result = recalculate_all_visible_filmstrips(true)
+			if(result) {
+				setStartAt(result.normalized_left)
+				setVisibleFilmstrips(result.visible)
+			}
 		})
 		return () => {}
 	})
@@ -49,34 +38,40 @@ export const Filmstrips = shadow_view({styles}, use => (effect: VideoEffect, tim
 		if(effect.kind === "video") {
 			ffmpeg.get_frames_count(effect.file).then(async frames => {
 				let generated_frames = 0
-				const placeholders = generate_loading_image_placeholders(frames)
-				setThumbnails(placeholders)
-				const visible = recalculate_all_visible_images(placeholders)
-				setVisibleThumbnails(visible)
+				const placeholders = generate_loading_filmstrip_placeholders(frames)
+				setFilmstrips(placeholders)
+				const result = recalculate_all_visible_filmstrips(true)
+				if(result) {
+					setVisibleFilmstrips(result.visible)
+					setStartAt(result.normalized_left)
+				}
 				for await(const frame_blob_url of ffmpeg.generate_video_effect_filmstrips(effect.file)) {
-					const updated_thumbnails = [...getThumbnails()]
+					const updated_thumbnails = [...getFilmstrips()]
 					updated_thumbnails[generated_frames] = frame_blob_url
-					setThumbnails(updated_thumbnails)
-					const visible = recalculate_all_visible_images(getThumbnails())
-					setVisibleThumbnails(visible)
+					setFilmstrips(updated_thumbnails)
+					const result = recalculate_all_visible_filmstrips(true)
+					if(result) {
+						setStartAt(result.normalized_left)
+						setVisibleFilmstrips(result.visible)
+					}
 					generated_frames += 1
 				}
 			})
 		}
 		return () => {
-			for(const url of getThumbnails()) {
+			for(const url of getFilmstrips()) {
 				URL.revokeObjectURL(url)
 			}
 		}
 	})
 
 	function get_filmstrip_at(position: number) {
-		const width_of_frame_if_all_frames = calculate_effect_width(effect, use.context.state.timeline.zoom) / getThumbnails().length
+		const width_of_frame_if_all_frames = calculate_effect_width(effect, use.context.state.timeline.zoom) / getFilmstrips().length
 		let start_at_filmstrip = position / width_of_frame_if_all_frames
 		return Math.floor(start_at_filmstrip)
 	}
 
-	function generate_loading_image_placeholders(frames: number) {
+	function generate_loading_filmstrip_placeholders(frames: number) {
 		const new_arr = []
 		for(let i = 0; i <= frames - 1; i++) {
 			new_arr.push(new URL("/assets/loading.svg", import.meta.url).toString())
@@ -84,24 +79,29 @@ export const Filmstrips = shadow_view({styles}, use => (effect: VideoEffect, tim
 		return new_arr
 	}
 
-	function recalculate_all_visible_images(images: string[]) {
+	function recalculate_all_visible_filmstrips(force_recalculate?: boolean) {
 		const new_arr = []
-		const width_of_frame = calculate_effect_width(effect, 2) / getThumbnails().length
+		const width_of_frame = calculate_effect_width(effect, 2) / getFilmstrips().length
 		const margin = width_of_frame * 2
 		const frame_count = timeline.clientWidth / 100
 		const effect_left = calculate_start_position(effect.start_at_position, use.context.state.timeline.zoom)
 		const normalized_left = Math.floor((timeline.scrollLeft - effect_left) / width_of_frame) * width_of_frame - margin < 0
-		? 0
-		: Math.floor((timeline.scrollLeft - effect_left) / width_of_frame) * width_of_frame - margin
-		for(let i = 0; i<= frame_count;i+=1) {
-			const effect_width = calculate_effect_width(effect, use.context.state.timeline.zoom)
-			const position = normalized_left + width_of_frame * i
-			if(position <= effect_width)
-				new_arr.push(getThumbnails()[get_filmstrip_at(position)])
+			? 0
+			: Math.floor((timeline.scrollLeft - effect_left) / width_of_frame) * width_of_frame - margin
+		const scroll_move_is_bigger_than_width_of_frame = Math.abs(normalized_left - getLastEffectOffseLeftPosition()) >= Math.floor(width_of_frame)
+		const should_load_new_filmstrips = force_recalculate
+			? true
+			: scroll_move_is_bigger_than_width_of_frame
+		if(should_load_new_filmstrips) {
+			for(let i = 0; i<= frame_count; i+=1) {
+				const effect_width = calculate_effect_width(effect, use.context.state.timeline.zoom)
+				const position = normalized_left + width_of_frame * i
+				if(position <= effect_width)
+					new_arr.push(getFilmstrips()[get_filmstrip_at(position)])
+			}
+			return {visible: new_arr, normalized_left}
 		}
-		setStartAt(normalized_left)
-		return new_arr
 	}
 
-	return html`${visibleThumbnails.map((thumbnail, i) => html`<img data-index=${i}  class="thumbnail" style="transform: translateX(${startAt}px);height: 40px; width: ${calculate_effect_width(effect, 2) / getThumbnails().length}px; pointer-events: none;" src=${thumbnail} />`)}`
+	return html`${visibleFilmstrips.map((filmstrip, i) => html`<img data-index=${i}  class="thumbnail" style="transform: translateX(${startAt}px);height: 40px; width: ${calculate_effect_width(effect, 2) / getFilmstrips().length}px; pointer-events: none;" src=${filmstrip} />`)}`
 })
