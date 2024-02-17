441ffa67ea789b510ef49f343e4f96146979a461
Przemek Galezki <przemekgg2002@gmail.com>
2024-02-17 22:32:49 +0100

    improve: filmstrips placement

--- s/components/omni-timeline/views/filmstrips/styles.ts
+++ s/components/omni-timeline/views/filmstrips/styles.ts
@@ -3,10 +3,15 @@ import {css} from "@benev/slate"
 export const styles = css`
 	:host {
 		height: 40px;
+		display: flex;
+		overflow: hidden;
+		width: 100%;
 	}
+
 	img {
 		display: inline;
 	}
+
 	svg {
 		display: inline;
 		height: 100%;
