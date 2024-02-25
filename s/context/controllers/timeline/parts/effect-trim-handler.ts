b13e2e838ff2fe8d4448fa28135e97374b4d883e
Przemek Galezki <przemekgg2002@gmail.com>
2024-02-25 23:32:39 +0100

    implement: effect trim handler

---
+++ s/context/controllers/timeline/parts/effect-trim-handler.ts
@@ -0,0 +1,59 @@
+import {ShockDragDrop} from "@benev/construct"
+
+import {TimelineActions} from "../actions.js"
+import {AnyEffect, XTimeline} from "../types.js"
+
+export class effectTrimHandler {
+	effect_resize_handle_drag = new ShockDragDrop<"right" | "left", {x: number}>({handle_drop: (_event: DragEvent) => {}})
+	initial_x = 0
+	initial_start_position = 0
+	initial_start = 0
+	initial_end = 0
+	side: "left" | "right" | null = null
+	effect: AnyEffect | null = null
+
+	constructor(private actions: TimelineActions) {}
+
+	effect_dragover(event: DragEvent, element: HTMLElement, timeline: XTimeline) {
+		if(!this.effect_resize_handle_drag.grabbed)
+			return
+		const get_effect = timeline.effects.find(({id}) => id === this.effect?.id)
+		const delta = event.clientX - this.initial_x;
+		if(this.effect_resize_handle_drag.grabbed === "left") {
+			const is_within_boundary = delta > -get_effect!.start
+			if (is_within_boundary) {
+				const start_at = (this.initial_start_position + (delta  * Math.pow(2, -timeline.zoom)))
+				const start = (this.initial_start + delta * Math.pow(2, -timeline.zoom))
+				this.actions.set_effect_start_position(this.effect!, start_at)
+				this.actions.set_effect_start(get_effect!, start)
+			}
+		} else {
+			const is_within_boundary = delta < get_effect!.duration - get_effect!.end
+			if (is_within_boundary) {
+				const end = (this.initial_end + delta * Math.pow(2, -timeline.zoom))
+				this.actions.set_effect_end(get_effect!, end)
+			}
+		}
+		this.effect_resize_handle_drag.dropzone.dragover({x: element.clientLeft})(event)
+	}
+
+	
+	trim_start = (e: DragEvent, effect: AnyEffect, side: "left" | "right") => {
+		this.initial_x = e.clientX
+		this.initial_start_position = effect.start_at_position
+		this.initial_start = effect.start
+		this.initial_end = effect.end
+		this.side = side
+		this.effect = effect
+		this.effect_resize_handle_drag.dragzone.dragstart(side)(e)
+	}
+
+	trim_end = (e: DragEvent) => {
+		this.effect_resize_handle_drag.dragzone.dragend()(e)
+	}
+
+	trim_drop = (e: DragEvent) => {
+		this.effect_resize_handle_drag.dropzone.drop(this.effect_resize_handle_drag.hovering!)(e)
+	}
+
+}
