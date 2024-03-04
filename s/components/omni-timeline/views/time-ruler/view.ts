f2f6560eef416526a4a5cc955afab2f27c165b98
Przemek Galezki <przemekgg2002@gmail.com>
2024-03-04 03:22:04 +0100

    wip: improve timeline time ruler

--- s/components/omni-timeline/views/time-ruler/view.ts
+++ s/components/omni-timeline/views/time-ruler/view.ts
@@ -2,10 +2,10 @@ import {GoldElement, html, watch} from "@benev/slate"
 
 import {styles} from "./styles.js"
 import {shadow_view} from "../../../../context/slate.js"
-import {pad_to_2_digits} from "./utils/pad_to_2_digits.js"
 
 export const TimeRuler = shadow_view({styles}, use => (timeline: GoldElement) => {
 	const [timeCodes, setTimeCodes] = use.state<{time: string, offset: number}[]>([])
+	const [_, setPrevTimecode, getPrevTimecode] = use.state<null | number>(null)
 
 	use.setup(() => {
 		const set_time_codes = () => setTimeCodes(generate_time_codes(use.context.state.timeline.zoom))
@@ -18,25 +18,84 @@ export const TimeRuler = shadow_view({styles}, use => (timeline: GoldElement) =>
 		let seconds = Math.floor(milliseconds / 1000)
 		let minutes = Math.floor(seconds / 60)
 		let hours = Math.floor(minutes / 60)
-
 		seconds = seconds % 60
 		minutes = minutes % 60
 		hours = hours % 24
-
-		return `${pad_to_2_digits(hours)}:${pad_to_2_digits(minutes)}:${pad_to_2_digits(seconds)}`
+		const zoom_rounded = roundToTwoDecimalPlaces(use.context.state.timeline.zoom)
+		if(zoom_rounded > -6) {
+			return `${milliseconds/1000}s`
+		} else {
+			return `${minutes}.${seconds}m`
+		}
 	}
+
+function roundToTwoDecimalPlaces(zoom: number) {
+  return Math.round(zoom * 100) / 100;
+}
+
+function round_timecode_to(timecode: number, zoom: number, ms: number) {
+	const closestTimeCode = Math.round(timecode / (ms * Math.ceil(-zoom))) * (ms * Math.ceil(-zoom))
+	const offset = closestTimeCode / Math.pow(2, -zoom);
+	// let seconds = Math.floor(timecode / 1000)
+	// seconds = seconds % 60
+	// console.log(closestTimeCode, seconds, ms, offset)
+	// console.log(zoom)
+	if(closestTimeCode !== getPrevTimecode()) {
+		setPrevTimecode(closestTimeCode)
+		return {
+			time: convert_ms_to_hms(closestTimeCode),
+			offset
+		}
+	}
+}
 
 	function generate_time_codes(zoom: number) {
 		const time_codes = []
-		const second = 100
-		let offset = second
-		offset += Math.pow(2, -zoom)
-
-		for(let time_code = timeline.scrollLeft; time_code <= timeline.scrollLeft + timeline.clientWidth; time_code+=offset) {
-			time_codes.push({
-				time: convert_ms_to_hms(time_code * Math.pow(2, -zoom)),
-				offset: time_code
-			})
+		const ms = 1000/use.context.state.timeline.timebase
+		for(let time_code = timeline.scrollLeft; time_code <= timeline.scrollLeft + timeline.clientWidth; time_code+=10) {
+			const exactTimeCode = time_code * Math.pow(2, -zoom)
+			const zoom_rounded = roundToTwoDecimalPlaces(zoom)
+			if(zoom_rounded < 0) {
+				if(zoom_rounded <= -12) {
+					const timecode = round_timecode_to(exactTimeCode, zoom, 60000)
+					if(timecode) time_codes.push(timecode)
+				}
+				if(zoom_rounded <= -10 && zoom_rounded > -12) {
+					const timecode = round_timecode_to(exactTimeCode, zoom, 30000)
+					if(timecode) time_codes.push(timecode)
+				}
+				if(zoom_rounded <= -8 && zoom_rounded > -10) {
+					const timecode = round_timecode_to(exactTimeCode, zoom, 10000)
+					if(timecode) time_codes.push(timecode)
+				}
+				if(zoom_rounded <= -6 && zoom_rounded > -8) {
+					const timecode = round_timecode_to(exactTimeCode, zoom, 5000)
+					if(timecode) time_codes.push(timecode)
+				}
+				if(zoom_rounded <= -4 && zoom_rounded > -6) {
+					const timecode = round_timecode_to(exactTimeCode, zoom, 1000)
+					if(timecode) time_codes.push(timecode)
+				}
+				if(zoom_rounded < -2 && zoom_rounded > -4) {
+					const timecode = round_timecode_to(exactTimeCode, zoom, 500)
+					if(timecode) time_codes.push(timecode)
+				}
+				if(zoom_rounded >= -2) {
+					const timecode = round_timecode_to(exactTimeCode, zoom, 100)
+					if(timecode) time_codes.push(timecode)
+				}
+			}
+			if(zoom_rounded >= 0) {
+				const closestTimeCode = Math.round(exactTimeCode / ms) * ms
+				const offset = closestTimeCode / Math.pow(2, -zoom)
+				if(closestTimeCode !== getPrevTimecode()) {
+					time_codes.push({
+						time: convert_ms_to_hms(closestTimeCode),
+						offset
+					})
+					setPrevTimecode(closestTimeCode)
+				}
+			} 
 		}
 		return time_codes
 	}
