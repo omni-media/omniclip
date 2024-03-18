2810b2019f8cfdb302d98ec8589bff5b0d29aad1
Przemek Galezki <przemekgg2002@gmail.com>
2024-03-18 02:12:34 +0100

    fix: binary accumulator, no longer stuck at invalid array length

--- s/context/controllers/video-export/tools/BinaryAccumulator/tool.ts
+++ s/context/controllers/video-export/tools/BinaryAccumulator/tool.ts
@@ -1,17 +1,25 @@
 export class BinaryAccumulator {
-	#bytes: number[] = []
+	#uints: Uint8Array[] = []
+	#size = 0
 
-	addChunk(chunk: Uint8Array) {
-		for(let i = 0; i <= chunk.length; i++) {
-			this.#bytes.push(chunk[i])
-		}
+	add_chunk(chunk: Uint8Array) {
+		this.#size += chunk.byteLength
+		this.#uints.push(chunk)
 	}
 
 	get binary() {
-		return new Uint8Array(this.#bytes)
+		let acc = 0
+		const binary = new Uint8Array(this.#size)
+		this.#uints.forEach(arr => {
+			binary.set(arr, acc)
+			acc += arr.length
+			//@ts-ignore
+			arr = null
+		})
+		return binary
 	}
 
 	clear_binary() {
-		this.#bytes = []
+		this.#uints = []
 	}
 }
