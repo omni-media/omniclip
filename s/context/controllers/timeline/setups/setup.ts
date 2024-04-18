14b13c618319ac911884d2487db78e78e278400f
Przemek Galezki <przemekgg2002@gmail.com>
2024-04-18 23:53:29 +0200

    tweak: timeline setup for tests

--- s/context/controllers/timeline/setups/setup.ts
+++ s/context/controllers/timeline/setups/setup.ts
@@ -3,19 +3,22 @@ import {SignalTower} from "@benev/slate/x/signals/tower.js"
 import {ZipAction} from "@benev/slate/x/watch/zip/action.js"
 
 import {Timeline} from "../controller.js"
-import {timeline_state} from "../state.js"
-import {timeline_actions} from "../actions.js"
+import {timeline_historical_state, timeline_non_historical_state} from "../state.js"
+import {timeline_historical_actions, timeline_non_historical_actions} from "../actions.js"
 import {OmniState} from "../../../../types.js"
+
+const actions = {...timeline_non_historical_actions, ...timeline_historical_actions}
+const state = {...timeline_historical_state, ...timeline_non_historical_state}
 
 export function setup() {
 	const signals = new SignalTower()
 	const watch = new WatchTower(signals)
 	const timelineTree = watch.stateTree<OmniState>({
-		timeline: timeline_state
+		timeline: state
 	})
-	const actions = ZipAction.actualize(timelineTree, timeline_actions)
+	const actions_timeline = ZipAction.actualize(timelineTree, actions)
 	return {
 		timelineTree,
-		timelineController: new Timeline(actions)
+		timelineController: new Timeline(actions_timeline)
 	}
 }
