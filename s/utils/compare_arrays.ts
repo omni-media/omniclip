c979dc28780c9f9df095603d3eee32fc24a34754
Przemek Galezki <przemekgg2002@gmail.com>
2024-04-25 00:48:18 +0200

    fix: compare arrays util

--- s/utils/compare_arrays.ts
+++ s/utils/compare_arrays.ts
@@ -1,14 +1,16 @@
-export function compare_arrays<T>(originalArray: T[], newArray: T[]) {
-	const add: T[] = []
-	const remove: T[] = []
+import {AnyEffect} from "../context/controllers/timeline/types.js"
+
+export function compare_arrays(originalArray: AnyEffect[], newArray: AnyEffect[]) {
+	const add = []
+	const remove = []
 
 	for (const item of newArray) {
-		if (!originalArray.includes(item)) {
+		if (!originalArray.find(({id}) => id === item.id)) {
 			add.push(item)
 		}
 	}
 	for (const item of originalArray) {
-		if (!newArray.includes(item)) {
+		if (!newArray.find(({id}) => id === item.id)) {
 			remove.push(item)
 		}
 	}
