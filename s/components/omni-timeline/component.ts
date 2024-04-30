05b0d19a20efd738d8ea735f98f1486b60731cd4
Przemek Galezki <przemekgg2002@gmail.com>
2024-04-30 08:14:59 +0200

    fix: rendering effects in map()

--- s/components/omni-timeline/component.ts
+++ s/components/omni-timeline/component.ts
@@ -1,12 +1,13 @@
 import {html} from "@benev/slate"
 
+import {repeat} from "lit-html/directives/repeat.js";
 import {styles} from "./styles.js"
 import {Clip} from "./views/clip/view.js"
 import {shadow_component} from "../../context/slate.js"
 
 export const OmniTimeline = shadow_component({styles}, use => {
 	use.watch(() => use.context.state.timeline)
-	const render_clips = () => use.context.state.timeline.clips.map((clip, id) => Clip([clip, id]))
+	const render_clips = () => repeat(use.context.state.timeline.clips, (clip) => clip.id, (clip, id) => Clip([clip, id]))
 
 	return html`
 		<div
