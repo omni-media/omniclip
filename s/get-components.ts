12b12e3e3691a7dba241e8a206a258157081663e
Przemek Galezki <przemekgg2002@gmail.com>
2024-05-21 00:28:39 +0200

    add: get-components and export components

---
+++ s/get-components.ts
@@ -0,0 +1,9 @@
+import {OmniText} from "./components/omni-text/component.js"
+import {OmniMedia} from "./components/omni-media/component.js"
+import {OmniTimeline} from "./components/omni-timeline/component.js"
+
+export const getComponents = () => ({
+	OmniText,
+	OmniTimeline,
+	OmniMedia
+})
---
+++ s/index.ts
@@ -0,0 +1,7 @@
+export {register_to_dom} from "@benev/slate"
+
+export * from "./get-components.js"
+
+export * from "./components/omni-timeline/component.js"
+export * from "./components/omni-media/component.js"
+export * from "./components/omni-text/component.js"
