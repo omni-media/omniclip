b5de24a4fbd4f460720e8d0b65d5fa83167afd33
zenkyu <przemekgg2002@gmail.com>
2024-07-24 13:39:37 +0200

    fix: timeline width calculation

--- s/components/omni-timeline/utils/calculate_timeline_width.ts
+++ s/components/omni-timeline/utils/calculate_timeline_width.ts
@@ -2,7 +2,7 @@ import {AnyEffect} from "../../../context/types.js"
 
 export function calculate_timeline_width (effects: AnyEffect[], zoom: number) {
 	const last_effect = [...effects].sort((a, b) => {
-		if(a.start_at_position + a.duration > b.start_at_position + b.duration) {
+		if(a.start_at_position + (a.end - a.start) > b.start_at_position + (b.end - b.start)) {
 			return -1
 		} else {
 			return 1
@@ -10,6 +10,6 @@ export function calculate_timeline_width (effects: AnyEffect[], zoom: number) {
 	})[0] as AnyEffect | undefined
 	//if width is already 3x the last effect position then stop zoom out
 	if(last_effect) {
-	 return ((last_effect.start_at_position + last_effect.duration) * Math.pow(2, zoom)) + ((last_effect.duration / 5) * Math.pow(2, zoom))
+	 return ((last_effect.start_at_position + (last_effect.end - last_effect.start)) * Math.pow(2, zoom)) + (((last_effect.end - last_effect.start) / 5) * Math.pow(2, zoom))
 	} else return 3000
 }
