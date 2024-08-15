91765bb88861a608e13af8aa761ccd9184ae9680
zenkyu <przemekgg2002@gmail.com>
2024-08-16 00:53:57 +0200

    fix: demuxer, use stable version

--- s/tools/demuxer.ts
+++ s/tools/demuxer.ts
@@ -12,7 +12,7 @@ export async function demuxer(
 	const webdemuxer = new WebDemuxer({
 		// ⚠️ you need to put the dist/wasm-files file in the npm package into a static directory like public
 		// making sure that the js and wasm in wasm-files are in the same directory
-		wasmLoaderPath: "https://cdn.jsdelivr.net/npm/web-demuxer@latest/dist/wasm-files/ffmpeg.min.js",
+		wasmLoaderPath: "https://cdn.jsdelivr.net/npm/web-demuxer@1.0.5/dist/wasm-files/ffmpeg.min.js",
 	})
 	await webdemuxer.load(file)
 	const config = await webdemuxer.getVideoDecoderConfig()
