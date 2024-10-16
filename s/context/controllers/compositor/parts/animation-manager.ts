3c48d367e10719c7438dd34c9aef8aa60eb7d22d
zenkyu <przemekgg2002@gmail.com>
2024-10-16 14:40:33 +0200

    switch: from animejs to gsap

--- s/context/controllers/compositor/parts/animation-manager.ts
+++ s/context/controllers/compositor/parts/animation-manager.ts
@@ -1,10 +1,10 @@
+import gsap from "gsap"
 import {pub} from "@benev/slate"
 import {FabricObject} from "fabric"
-import anime from "animejs/lib/anime.es.js"
 
 import {Compositor} from "../controller.js"
 import {AnyEffect, ImageEffect, State, VideoEffect} from "../../../types.js"
-import { calculateProjectDuration } from "../../../../utils/calculate-project-duration.js"
+import {calculateProjectDuration} from "../../../../utils/calculate-project-duration.js"
 
 interface AnimationBase<T = AnimationIn | AnimationOut> {
 	targetEffect: VideoEffect | ImageEffect
@@ -19,9 +19,9 @@ export type AnimationNone = AnimationBase<(typeof animationNone)[number]>
 export type Animation = AnimationIn | AnimationOut
 
 export class AnimationManager {
-	timeline = anime.timeline({
-		duration: 10000,
-		autoplay: false,
+	timeline = gsap.timeline({
+		duration: 10, // GSAP uses seconds instead of milliseconds
+		paused: true,
 	})
 
 	#animations: Animation[] = []
@@ -30,63 +30,41 @@ export class AnimationManager {
 	constructor(private compositor: Compositor) {}
 
 	selectedAnimationForEffect(effect: AnyEffect | null, animation: Animation) {
-		if(!effect) return
-		const haveAnimation = this.#animations.find(anim => anim.targetEffect.id === effect.id && animation.type === anim.type)
-		if(haveAnimation) {
-			return true
-		} else return false
+		if (!effect) return
+		const haveAnimation = this.#animations.find(
+			(anim) => anim.targetEffect.id === effect.id && animation.type === anim.type
+		)
+		return !!haveAnimation
 	}
 
 	isAnyAnimationInSelected(effect: AnyEffect | null) {
-		if(!effect) return
-		const haveAnimation = this.#animations.find(animation => animation.targetEffect.id === effect.id && animation.type.includes("In"))
-		if(haveAnimation) {
-			return true
-		} else false
+		if (!effect) return false
+		const haveAnimation = this.#animations.find(
+			(animation) => animation.targetEffect.id === effect.id && animation.type.includes("In")
+		)
+		return !!haveAnimation
 	}
 
 	isAnyAnimationOutSelected(effect: AnyEffect | null) {
-		if(!effect) return
-		const haveAnimation = this.#animations.find(animation => animation.targetEffect.id === effect.id && animation.type.includes("Out"))
-		if(haveAnimation) {
-			return true
-		} else false
+		if (!effect) return false
+		const haveAnimation = this.#animations.find(
+			(animation) => animation.targetEffect.id === effect.id && animation.type.includes("Out")
+		)
+		return !!haveAnimation
 	}
 
 	updateTimelineDuration(duration: number) {
-		this.timeline.duration = duration
+		this.timeline.duration(duration / 1000) // GSAP duration in seconds
 	}
 
 	#getObject(effect: VideoEffect | ImageEffect) {
 		const videoObject = this.compositor.managers.videoManager.get(effect.id)
 		const imageObject = this.compositor.managers.imageManager.get(effect.id)
-		if(videoObject) {
+		if (videoObject) {
 			return videoObject
-		} else if(imageObject) {
+		} else if (imageObject) {
 			return imageObject
 		}
-	}
-
-	refreshAnimations(
-		effects: AnyEffect[],
-		state: State
-	) {
-		anime.remove(this.timeline)
-		this.timeline = anime.timeline({
-			duration: this.timeline.duration,
-			autoplay: false,
-		})
-		const updated = this.#animations.map(animation => {
-			const effect = effects.find(effect => effect.id === animation.targetEffect.id) as ImageEffect | VideoEffect | undefined
-			return {
-			...animation,
-			targetEffect: effect ?? animation.targetEffect
-		}})
-		this.#animations = updated
-		this.#animations.forEach(animation => {
-			this.removeAnimationFromEffect(animation.targetEffect, animation.type.includes("In") ? "In" : "Out", state)
-			this.addAnimationToEffect(animation.targetEffect, animation)
-		})
 	}
 
 	addAnimationToEffect(
@@ -94,47 +72,62 @@ export class AnimationManager {
 		animation: AnimationIn | AnimationOut,
 		state?: State
 	) {
-		if(state) {this.timeline.duration = calculateProjectDuration(state.effects)}
-		const object = this.#getObject(effect)
+		if (state) {
+			this.timeline.duration(calculateProjectDuration(state.effects) / 1000)
+		}
+
+		const object = this.#getObject(effect)!
 		this.#animations.push(animation)
+
 		switch (animation.type) {
 			case "slideIn": {
-				const targetPosition = {left: effect.rect.position_on_canvas.x}
-				const startPosition = {left: -effect.rect.width}
-				this.timeline.add({
-					targets: object,
-					duration: 1000,
-					left: [startPosition.left, targetPosition.left],
-					easing: "linear",
-					update: () => this.compositor.canvas.renderAll()
-				},
-					effect.start_at_position,
+				const targetPosition = { left: effect.rect.position_on_canvas.x }
+				const startPosition = { left: -effect.rect.width }
+
+				// Set the start position before animation begins
+				gsap.set(object, { left: startPosition.left })
+
+				// Animate the object to the target position
+				this.timeline.add(
+					gsap.to(object, {
+						duration: 1,
+						left: targetPosition.left,
+						ease: "linear",
+						onUpdate: () => this.compositor.canvas.renderAll()
+					}),
+					effect.start_at_position / 1000
 				)
+
 				break
 			}
+
 			case "slideOut": {
-				const targetPosition = {left: effect.rect.width}
-				const startPosition = {left: effect.rect.position_on_canvas.x}
-				this.timeline.add({
-					targets: object,
-					duration: 1000,
-					left: [startPosition.left, targetPosition.left],
-					easing: "linear",
-					update: () => this.compositor.canvas.renderAll(),
-				},
-					effect.start_at_position + (effect.end - effect.start) - 1000,
+				const targetPosition = { left: effect.rect.width }
+				const startPosition = { left: effect.rect.position_on_canvas.x }
+
+				// Set the start position before animation begins
+				gsap.set(object, { left: startPosition.left })
+
+				// Animate the object to the target position
+				this.timeline.add(
+					gsap.to(object, {
+						duration: 1,
+						left: targetPosition.left,
+						ease: "linear",
+						onUpdate: () => this.compositor.canvas.renderAll()
+					}),
+					(effect.start_at_position + (effect.end - effect.start) - 1000) / 1000
 				)
+
 				break
 			}
 		}
+
 		this.onChange.publish(true)
 	}
 
 	play(time: number) {
-		// console.log(this.timeline.currentTime, this.timeline.progress, "PLAYI", time)
-		// this.timeline.seek(time)
-		// this.timeline.play()
-		// console.log("anim plays", this.timeline)
+		this.timeline.play(time / 1000)
 	}
 
 	pause() {
@@ -142,11 +135,11 @@ export class AnimationManager {
 	}
 
 	seek(time: number) {
-		this.timeline.seek(time)
+		this.timeline.seek(time / 1000)
 	}
 
 	updateAnimationTimelineDuration(duration: number) {
-		this.timeline.duration = duration
+		this.timeline.duration(duration / 1000)
 	}
 
 	#resetObjectProperties(fabric: FabricObject, effect: ImageEffect | VideoEffect) {
@@ -162,20 +155,18 @@ export class AnimationManager {
 	removeAnimationFromEffect(effect: ImageEffect | VideoEffect, type: "In" | "Out", state: State) {
 		const object = this.#getObject(effect)
 		this.#resetObjectProperties(object!, effect)
-		anime.remove(object!)
-		const filtered = this.#animations.filter(animation => !(animation.targetEffect.id === effect.id && animation.type.includes(type)))
-		this.#animations = filtered
-		this.refreshAnimations(state.effects, state)
+		gsap.killTweensOf(object!)
+		this.#animations = this.#animations.filter(
+			(animation) => !(animation.targetEffect.id === effect.id && animation.type.includes(type))
+		)
 		this.onChange.publish(true)
 	}
 
 	removeAllAnimationsFromEffect(effect: ImageEffect | VideoEffect, state: State) {
 		const object = this.#getObject(effect)
 		this.#resetObjectProperties(object!, effect)
-		anime.remove(object!)
-		const filtered = this.#animations.filter(animation => !(animation.targetEffect.id === effect.id))
-		this.#animations = filtered
-		this.refreshAnimations(state.effects, state)
+		gsap.killTweensOf(object!)
+		this.#animations = this.#animations.filter((animation) => animation.targetEffect.id !== effect.id)
 		this.onChange.publish(true)
 	}
 }
