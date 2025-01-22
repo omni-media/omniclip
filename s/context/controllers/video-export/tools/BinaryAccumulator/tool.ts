5fa48f6f04c75ca5cc9a7d40cbe3ce8808adfa2e
zenkyu <przemekgg2002@gmail.com>
2025-01-22 23:50:24 +0100

    improve: binary accumulator

--- s/context/controllers/video-export/tools/BinaryAccumulator/tool.ts
+++ s/context/controllers/video-export/tools/BinaryAccumulator/tool.ts
@@ -1,25 +1,40 @@
 export class BinaryAccumulator {
 	#uints: Uint8Array[] = []
 	#size = 0
+	#cachedBinary: Uint8Array | null = null
 
 	add_chunk(chunk: Uint8Array) {
 		this.#size += chunk.byteLength
 		this.#uints.push(chunk)
+		this.#cachedBinary = null  // Invalidate cache on new data
 	}
 
+	// try to get binary once at the end of something otherwise it will cause some memory leaks
+	// in most cases getting size should be enough
 	get binary() {
-		let acc = 0
+		// Return cached binary if available
+		if (this.#cachedBinary) {
+			return this.#cachedBinary
+		}
+
+		let offset = 0
 		const binary = new Uint8Array(this.#size)
-		this.#uints.forEach(arr => {
-			binary.set(arr, acc)
-			acc += arr.length
-			//@ts-ignore
-			arr = null
-		})
+		for (const chunk of this.#uints) {
+			binary.set(chunk, offset)
+			offset += chunk.byteLength
+		}
+
+		this.#cachedBinary = binary  // Cache the result
 		return binary
+	}
+
+	get size() {
+		return this.#size
 	}
 
 	clear_binary() {
 		this.#uints = []
+		this.#size = 0
+		this.#cachedBinary = null
 	}
 }
