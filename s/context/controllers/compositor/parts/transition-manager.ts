c7edc216f633419202c616c9b412131e12d39a2a
zenkyu <przemekgg2002@gmail.com>
2025-02-07 02:38:37 +0100

    refactor: transition manager

--- s/context/controllers/compositor/parts/transition-manager.ts
+++ s/context/controllers/compositor/parts/transition-manager.ts
@@ -1,13 +1,13 @@
-import {Actions} from "../../../actions.js"
-import {Compositor} from "../controller.js"
-import {omnislate} from "../../../context.js"
-import {AnimationManager} from "./animation-manager.js"
-import {AnyEffect, ImageEffect, State, VideoEffect} from "../../../types.js"
-import {normalizeTransitionDuration} from "../../../../components/omni-transitions/utils/normalize-transition-duration.js"
+import {Actions} from '../../../actions.js'
+import {Compositor} from '../controller.js'
+import {omnislate} from '../../../context.js'
+import {AnimationManager} from './animation-manager.js'
+import {AnyEffect, ImageEffect, State, VideoEffect} from '../../../types.js'
+import {normalizeTransitionDuration} from '../../../../components/omni-transitions/utils/normalize-transition-duration.js'
 
-export const transition = ["fade"] as const
+export const transition = ['fade'] as const
 export type TransitionAbleEffect = ImageEffect | VideoEffect
-type TransitionAnimation = "fade"
+type TransitionAnimation = 'fade'
 
 export type SelectedPair = {
 	incoming: TransitionAbleEffect
@@ -22,65 +22,84 @@ interface Transition {
 }
 
 interface PropsToUpdate {
+	effect: ImageEffect | VideoEffect
 	duration: number
 }
 
 export class TransitionManager extends AnimationManager {
 	selected: null | SelectedPair = null
 
-	constructor(compositor: Compositor, actions: Actions) {
-		super(compositor, actions, "Transition")
+	constructor (compositor: Compositor, actions: Actions) {
+		super(compositor, actions, 'Transition')
 	}
 
-	clearTransitions() {
-		this.clearAnimations()
+	clearTransitions (omit?: boolean) {
+		this.clearAnimations(omit)
 		this.selected = null
 	}
 
-	isSelected(kind: TransitionAnimation) {
-		if(this.selected) {
-			return this.getAnyAnimation(this.selected.incoming)?.name.includes(kind)
-		} else false
+	isSelected (kind: TransitionAnimation) {
+		if (this.selected) {
+			return !!this.getAnyAnimation(this.selected.incoming)?.name.includes(kind)
+		}
+		return false
 	}
 
-	selectTransition(transition: Transition) {
+	selectTransition (transition: Transition) {
+		const halfDuration = transition.duration / 2
 		this.selected = {
 			incoming: transition.incoming,
 			outgoing: transition.outgoing
 		}
-		if(!this.isAnyAnimationInSelected(transition.incoming)) {
-			this.actions.set_effect_start_position(transition.incoming, transition.incoming.start_at_position - transition.duration / 2)
-			this.actions.set_effect_start(transition.incoming, transition.incoming.start + transition.duration / 2)
+		if (!this.isAnyAnimationInSelected(transition.incoming)) {
+			this.actions.set_effect_start_position(
+				transition.incoming,
+				transition.incoming.start_at_position - halfDuration
+			)
+			this.actions.set_effect_start(
+				transition.incoming,
+				transition.incoming.start + halfDuration
+			)
 		}
-		if(!this.isAnyAnimationOutSelected(transition.outgoing)) {
-			this.actions.set_effect_end(transition.outgoing, transition.outgoing.end - transition.duration / 2)
+		if (!this.isAnyAnimationOutSelected(transition.outgoing)) {
+			this.actions.set_effect_end(
+				transition.outgoing,
+				transition.outgoing.end - halfDuration
+			)
 		}
 
 		this.onChange.publish(0)
 
+		// Return an object with an apply method that re-reads state and schedules the tweens.
+		// This ensures that on the receiving side the latest effect properties are used.
 		return {
-			apply: (state: State) => {
-				if(!this.isAnyAnimationInSelected(transition.incoming)) {
-					const effect = state.effects.find(e => e.id === transition.incoming.id) as VideoEffect | ImageEffect
-					this.selectAnimation(
-						effect,
-						{targetEffect: transition.incoming,
-						type: "in",
-						duration: transition.duration,
-						name: "fade-in",
-						for: "Transition"},
+			apply: async (state: State) => {
+				const incomingEffect = state.effects.find(e => e.id === transition.incoming.id) as VideoEffect | ImageEffect
+				const outgoingEffect = state.effects.find(e => e.id === transition.outgoing.id) as VideoEffect | ImageEffect
+
+				if (!this.isAnyAnimationInSelected(incomingEffect)) {
+					await this.selectAnimation(
+						incomingEffect,
+						{
+							targetEffect: incomingEffect,
+							type: 'in',
+							duration: transition.duration,
+							name: 'fade-in',
+							for: 'Transition'
+						},
 						state
 					)
 				}
-				if(!this.isAnyAnimationOutSelected(transition.outgoing)) {
-					const effect = state.effects.find(e => e.id === transition.outgoing.id) as VideoEffect | ImageEffect
-					this.selectAnimation(
-						effect,
-						{targetEffect: transition.outgoing,
-						type: "out",
-						duration: transition.duration,
-						name: "fade-out",
-						for: "Transition"},
+				if (!this.isAnyAnimationOutSelected(outgoingEffect)) {
+					await this.selectAnimation(
+						outgoingEffect,
+						{
+							targetEffect: outgoingEffect,
+							type: 'out',
+							duration: transition.duration,
+							name: 'fade-out',
+							for: 'Transition'
+						},
 						state
 					)
 				}
@@ -88,43 +107,107 @@ export class TransitionManager extends AnimationManager {
 		}
 	}
 
-	async updateTransition(state: State, propsToUpdate?: PropsToUpdate) {
-		if(this.selected && propsToUpdate) {
+	async updateTransition (state: State, propsToUpdate?: PropsToUpdate) {
+		if (this.selected && propsToUpdate) {
+			const fullDuration = normalizeTransitionDuration(propsToUpdate.duration, 1000 / state.timebase)
+			const halfDuration = fullDuration / 2
+
 			const {incoming, outgoing} = this.selected
-			const newDuration = normalizeTransitionDuration(propsToUpdate.duration, 1000/state.timebase)
-			await this.refreshOne(state, incoming, propsToUpdate, "in", false)
-			await this.refreshOne(state, outgoing, propsToUpdate, "out", false)
-			this.actions.set_animation_duration(newDuration, incoming)
-			this.actions.set_animation_duration(newDuration, outgoing)
-			this.actions.set_effect_start_position(incoming, incoming.start_at_position - newDuration / 2)
-			this.actions.set_effect_start(incoming, incoming.start + newDuration / 2)
-			this.actions.set_effect_end(outgoing, outgoing.end - newDuration / 2)
+			this.actions.set_animation_duration(fullDuration, outgoing, "Transition")
+			this.actions.set_animation_duration(fullDuration, incoming, "Transition")
+			this.actions.set_effect_start_position(
+				incoming,
+				incoming.start_at_position - halfDuration
+			)
+			this.actions.set_effect_start(
+				incoming,
+				incoming.start + halfDuration
+			)
+			this.actions.set_effect_end(
+				outgoing,
+				outgoing.end - halfDuration
+			)
 		}
 	}
 
-	removeSelectedTransition() {
-		const {outgoing, incoming} = this.selected!
-		this.deselectAnimation(incoming, "in")
-		this.deselectAnimation(outgoing, "out")
-		this.selected = null
+	removeSelectedTransition () {
+		if (this.selected) {
+			const {incoming, outgoing} = this.selected
+			this.deselectAnimation(incoming, 'in')
+			this.deselectAnimation(outgoing, 'out')
+			this.selected = null
+		}
 	}
 
-	removeAllTransitions(effect: AnyEffect) {
+	removeAllTransitions (effect: AnyEffect) {
 		const animations = this.getAnimations(effect)
 		animations.forEach(a => this.deselectAnimation(a.targetEffect, a.type))
 	}
 
-	async removeTransition(effect: AnyEffect, kind: "incoming" | "outgoing") {
-		const animation = this.getAnimation(effect, kind === "incoming" ? "in" : "out")
-		if(animation) {
-			if(animation.targetEffect.id === this.selected?.incoming.id ||
-				animation.targetEffect.id === this.selected?.outgoing.id) {this.selected = null}
-			await this.deselectAnimation(animation.targetEffect, kind === "incoming" ? "in" : "out")
+	async removeTransition (effect: AnyEffect, kind: 'incoming' | 'outgoing') {
+		const animation = this.getAnimation(effect, kind === 'incoming' ? 'in' : 'out')
+		if (animation) {
+			if (
+				animation.targetEffect.id === this.selected?.incoming.id ||
+				animation.targetEffect.id === this.selected?.outgoing.id
+			) {
+				this.selected = null
+			}
+			await this.deselectAnimation(animation.targetEffect, kind === 'incoming' ? 'in' : 'out')
 		}
 	}
 
-	getTransition(effect: AnyEffect) {
+	getTransition (effect: AnyEffect) {
 		return this.getAnyAnimation(effect)
+	}
+
+	getTransitionDuration (effect: AnyEffect) {
+		const transitionAnimation = this.getTransition(effect)
+		let incoming = 0
+		let outgoing = 0
+		if (transitionAnimation) {
+			incoming = transitionAnimation.duration / 2
+			outgoing = transitionAnimation.duration / 2
+		}
+		return {incoming, outgoing}
+	}
+
+	getTransitions () {
+		return omnislate.context.state.animations.map(a => ({
+			...a,
+			duration: a.duration / 2
+		}))
+	}
+
+	findTouchingClips (clips: AnyEffect[]) {
+		const clipsByTrack: Record<number, AnyEffect[]> = clips.reduce((acc, clip) => {
+			acc[clip.track] = acc[clip.track] || []
+			acc[clip.track].push(clip)
+			return acc
+		}, {} as Record<number, AnyEffect[]>)
+
+		const touchingClips: {outgoing: TransitionAbleEffect; incoming: TransitionAbleEffect; position: number}[] = []
+
+		Object.values(clipsByTrack).forEach(trackClips => {
+			const sortedClips = trackClips.sort((a, b) => a.start_at_position - b.start_at_position)
+			for (let i = 0; i < sortedClips.length - 1; i++) {
+				const currentClip = sortedClips[i]
+				const nextClip = sortedClips[i + 1]
+				const currentClipEnd = currentClip.start_at_position + (currentClip.end - currentClip.start)
+				if (
+					currentClipEnd === nextClip.start_at_position &&
+					['image', 'video'].includes(currentClip.kind) &&
+					['image', 'video'].includes(nextClip.kind)
+				) {
+					touchingClips.push({
+						outgoing: currentClip as TransitionAbleEffect,
+						incoming: nextClip as TransitionAbleEffect,
+						position: nextClip.start_at_position
+					})
+				}
+			}
+		})
+		return touchingClips
 	}
 
 	removeTransitionFromNoLongerTouchingEffects(state: State) {
@@ -141,61 +224,5 @@ export class TransitionManager extends AnimationManager {
 				await this.removeTransition(transition.targetEffect, transition.type === "in" ? "incoming" : "outgoing")
 			}
 		})
-	}
-
-	getTransitions() {
-		return omnislate.context.state.animations.map(a => ({
-			...a,
-			duration: a.duration / 2
-		}))
-	}
-
-	getTransitionDuration(effect: AnyEffect) {
-		const transition = this.getTransition(effect)
-		let incoming = 0
-		let outgoing = 0
-		if(transition) {
-			if(transition.type === "in") {
-				incoming = transition.duration / 2
-			} else {
-				outgoing = transition.duration / 2
-			}
-		}
-		return {incoming, outgoing}
-	}
-
-
-	findTouchingClips(clips: AnyEffect[]) {
-		const clipsByTrack: Record<number, AnyEffect[]> = clips.reduce((acc, clip) => {
-			acc[clip.track] = acc[clip.track] || []
-			acc[clip.track].push(clip)
-			return acc
-		}, {} as Record<number, AnyEffect[]>)
-
-		const touchingClips: {outgoing: TransitionAbleEffect; incoming: TransitionAbleEffect; position: number}[] = []
-
-		Object.values(clipsByTrack).forEach(trackClips => {
-			const sortedClips = trackClips.sort((a, b) => a.start_at_position - b.start_at_position)
-
-			for (let i = 0; i < sortedClips.length - 1; i++) {
-				const currentClip = sortedClips[i]
-				const nextClip = sortedClips[i + 1]
-				const currentClipEndPosition = currentClip.start_at_position + (currentClip.end - currentClip.start)
-
-				if (
-					currentClipEndPosition === nextClip.start_at_position &&
-					['image', 'video'].includes(currentClip.kind) &&
-					['image', 'video'].includes(nextClip.kind)
-				) {
-					touchingClips.push({
-						outgoing: currentClip as TransitionAbleEffect,
-						incoming: nextClip as TransitionAbleEffect,
-						position: nextClip.start_at_position
-					})
-				}
-			}
-		})
-
-		return touchingClips
 	}
 }
