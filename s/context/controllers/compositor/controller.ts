d039ee30510d088e19c5ab9a58ccd635bd646890
zenkyu <przemekgg2002@gmail.com>
2025-02-20 11:05:45 +0100

    wip: compositor fabricjs to pixijs

--- s/context/controllers/compositor/controller.ts
+++ s/context/controllers/compositor/controller.ts
@@ -1,29 +1,29 @@
 import {pub, reactor, signal} from "@benev/slate"
-import {Canvas, Rect} from "fabric/dist/fabric.mjs"
+import {Application, Graphics, Container, FederatedPointerEvent, Rectangle} from "pixi.js/dist/pixi.mjs"
 
 import {Actions} from "../../actions.js"
 import {Media} from "../media/controller.js"
-import {TextManager} from "./parts/text-manager.js"
+// import {TextManager} from "./parts/text-manager.js"
 import {ImageManager} from "./parts/image-manager.js"
 import {AudioManager} from "./parts/audio-manager.js"
 import {VideoManager} from "./parts/video-manager.js"
-import {FiltersManager} from "./parts/filter-manager.js"
 import {AlignGuidelines} from "./lib/aligning_guidelines.js"
+// import {FiltersManager} from "./parts/filter-manager.js"
 import {AnyEffect, AudioEffect, State} from "../../types.js"
-import {AnimationManager} from "./parts/animation-manager.js"
+// import {AnimationManager} from "./parts/animation-manager.js"
 import {compare_arrays} from "../../../utils/compare_arrays.js"
-import {TransitionManager} from "./parts/transition-manager.js"
-import {sort_effects_by_track} from "../video-export/utils/sort_effects_by_track.js"
+// import {TransitionManager} from "./parts/transition-manager.js"
+// import {sort_effects_by_track} from "../video-export/utils/sort_effects_by_track.js"
 import {get_effect_at_timestamp} from "../video-export/utils/get_effect_at_timestamp.js"
 
 export interface Managers {
 	videoManager: VideoManager
-	textManager: TextManager
+	// textManager: TextManager
 	imageManager: ImageManager
 	audioManager: AudioManager
-	animationManager: AnimationManager
-	filtersManager: FiltersManager
-	transitionManager: TransitionManager
+	// animationManager: AnimationManager
+	// filtersManager: FiltersManager
+	// transitionManager: TransitionManager
 }
 
 export class Compositor {
@@ -34,28 +34,40 @@ export class Compositor {
 	timecode = 0
 	timebase = 25
 	currently_played_effects = new Map<string, AnyEffect>()
-	
-	canvas_element = document.createElement("canvas")
-	canvas: Canvas
-	ctx = this.canvas_element.getContext("2d")
+
+	app = new Application()
 	#seekedResolve: ((value: unknown) => void) | null = null
 	
 	managers: Managers
 
+	selectedElement: Container | null = null
+	graphics = new Graphics() // graphics for align guidelines
+	guidelines: AlignGuidelines | null = null
+
+
 	constructor(private actions: Actions) {
-	this.canvas = new Canvas(this.canvas_element, {width: 1920, height: 1080, renderOnAddRemove: true, preserveObjectStacking: true, imageSmoothingEnabled: false})
-	this.init_guidelines()
 	this.#on_new_canvas_object_set_handle_styles()
 	this.#on_selected_canvas_object()
-
+	this.app.init({width: 1920, height: 1080}).then(() => {
+		this.guidelines = new AlignGuidelines({
+			app: this.app,
+			compositor: this
+		})
+		this.init_guidelines()
+		this.app.stage.eventMode = "static"
+		this.app.stage.hitArea = this.app.screen
+		this.app.stage.addChild(this.graphics)
+		this.app.stage.on('pointerup', this.canvasElementDrag.onDragEnd);
+		this.app.stage.on('pointerupoutside', this.canvasElementDrag.onDragEnd);
+	})
 	this.managers = {
 		videoManager: new VideoManager(this, actions),
-		textManager: new TextManager(this, actions),
+		// textManager: new TextManager(this, actions),
 		imageManager: new ImageManager(this, actions),
 		audioManager: new AudioManager(this, actions),
-		animationManager: new AnimationManager(this, actions, "Animation"),
-		filtersManager: new FiltersManager(this, actions),
-		transitionManager: new TransitionManager(this, actions)
+		// animationManager: new AnimationManager(this, actions, "Animation"),
+		// filtersManager: new FiltersManager(this, actions),
+		// transitionManager: new TransitionManager(this, actions)
 	}
 
 		this.#on_playing()
@@ -63,13 +75,13 @@ export class Compositor {
 			() => this.#is_playing.value,
 			(is_playing) => {
 				if(is_playing) {
-					this.managers.transitionManager.play(this.timecode)
-					this.managers.animationManager.play(this.timecode)
+					// this.managers.transitionManager.play(this.timecode)
+					// this.managers.animationManager.play(this.timecode)
 					this.managers.videoManager.play_videos()
 					this.managers.audioManager.play_audios()
 				} else {
-					this.managers.transitionManager.pause()
-					this.managers.animationManager.pause()
+					// this.managers.transitionManager.pause()
+					// this.managers.animationManager.pause()
 					this.managers.videoManager.pause_videos()
 					this.managers.audioManager.pause_audios()
 				}
@@ -90,16 +102,40 @@ export class Compositor {
 		requestAnimationFrame(this.#on_playing)
 	}
 
+	canvasElementDrag = {
+		onDragStart: (e: FederatedPointerEvent, element: Container) => {
+			let position = e.getLocalPosition(element)
+			element.pivot.set(position.x, position.y)
+			element.position.set(e.global.x, e.global.y)
+			this.selectedElement = element
+			this.app.stage.on('pointermove', (e) => this.canvasElementDrag.onDragMove(e))
+		},
+		onDragEnd: () => {
+			if (this.selectedElement) {
+				this.app.stage.off('pointermove', this.canvasElementDrag.onDragMove)
+				this.selectedElement = null
+			}
+		},
+		onDragMove: (event: FederatedPointerEvent) => {
+			this.selectedElement?.parent.toLocal(event.global, undefined, this.selectedElement.position)
+			if (this.selectedElement) {
+				if(this.guidelines) {
+					this.guidelines.on_object_move_or_scale(event)
+				}
+			}
+		}
+	}
+
 	reset() {
 		this.currently_played_effects.clear()
-		this.canvas.clear()
+		this.app.renderer.clear()
 	}
 
 	clear(omit?: boolean) {
-		this.canvas.clear()
+		this.app.renderer.clear()
 		this.init_guidelines()
-		this.managers.animationManager.clearAnimations(omit)
-		this.managers.transitionManager.clearTransitions(omit)
+		// this.managers.animationManager.clearAnimations(omit)
+		// this.managers.transitionManager.clearTransitions(omit)
 	}
 	
 	#calculate_elapsed_time() {
@@ -112,7 +148,7 @@ export class Compositor {
 	compose_effects(effects: AnyEffect[], timecode: number, exporting?: boolean) {
 		this.timecode = timecode
 		this.#update_currently_played_effects(effects, timecode, exporting)
-		this.canvas.requestRenderAll()
+		this.app.render()
 	}
 
 	get_effect_current_time_relative_to_timecode(effect: AnyEffect, timecode: number) {
@@ -120,20 +156,27 @@ export class Compositor {
 		return current_time / 1000
 	}
 
+	// get_effects_relative_to_timecode(effects: AnyEffect[], timecode: number) {
+	// 	return effects.filter(effect => {
+	// 		// const {incoming, outgoing} = this.managers.transitionManager.getTransitionDuration(effect)
+	// 		// return effect.start_at_position - incoming <= timecode && timecode <= effect.start_at_position + (effect.end - effect.start) + outgoing
+	// 	})
+	// }
+
 	get_effects_relative_to_timecode(effects: AnyEffect[], timecode: number) {
 		return effects.filter(effect => {
-			const {incoming, outgoing} = this.managers.transitionManager.getTransitionDuration(effect)
-			return effect.start_at_position - incoming <= timecode && timecode <= effect.start_at_position + (effect.end - effect.start) + outgoing
+			return effect.start_at_position <= timecode && timecode <= effect.start_at_position + (effect.end - effect.start)
 		})
 	}
 
 	#update_currently_played_effects(effects: AnyEffect[], timecode: number, exporting?: boolean) {
 		const effects_relative_to_timecode = this.get_effects_relative_to_timecode(effects, timecode)
 		const {add, remove} = compare_arrays([...this.currently_played_effects.values()], effects_relative_to_timecode)
+		console.log(add, remove, "a")
 		this.#update_effects(effects_relative_to_timecode)
 		this.#remove_effects_from_canvas(remove, exporting)
 		this.#add_effects_to_canvas(add)
-		this.#draw_in_correct_order(sort_effects_by_track(effects_relative_to_timecode))
+		this.app.stage.sortChildren()
 	}
 
 	#update_effects(new_effects: AnyEffect[]) {
@@ -141,29 +184,9 @@ export class Compositor {
 		new_effects.forEach(effect => {this.currently_played_effects.set(effect.id, effect)})
 	}
 
-	#draw_in_correct_order(effects: AnyEffect[]) {
-		const max_track = 4 // lower track means it should draw on top of higher tracks, although moveObjectTo z-index works in reverse
-		for(const effect of effects) {
-			if(effect.kind === "image") {
-				const image = this.managers.imageManager.get(effect.id)
-				if(image)
-					this.canvas.moveObjectTo(image, max_track - effect.track)
-			}
-			else if(effect.kind === "video") {
-				const video = this.managers.videoManager.get(effect.id)
-				if(video)
-					this.canvas.moveObjectTo(video, max_track - effect.track)
-			}
-			else if(effect.kind === "text") {
-				const text = this.managers.textManager.get(effect.id)!
-				this.canvas.moveObjectTo(text, max_track - effect.track)
-			}
-		}
-	}
-
 	async seek(timecode: number, redraw?: boolean) {
-		this.managers.animationManager.seek(timecode)
-		this.managers.transitionManager.seek(timecode)
+		// this.managers.animationManager.seek(timecode)
+		// this.managers.transitionManager.seek(timecode)
 		for(const effect of this.currently_played_effects.values()) {
 			if(effect.kind === "audio") {
 				const audio = this.managers.audioManager.get(effect.id)
@@ -175,9 +198,9 @@ export class Compositor {
 				}
 			}
 			if(effect.kind === "video") {
-				this.managers.filtersManager.onseek(effect)
+				// this.managers.filtersManager.onseek(effect)
 				const video = this.managers.videoManager.get(effect.id)
-				const element = video?._originalElement as HTMLVideoElement | null
+				const element = video?.texture.source.resource as any as HTMLVideoElement | null
 				if(!redraw && element?.paused && this.#is_playing.value) {await element.play()}
 				if(redraw && timecode && element) {
 					const current_time = this.get_effect_current_time_relative_to_timecode(effect, timecode)
@@ -212,12 +235,12 @@ export class Compositor {
 			else if(effect.kind === "video") {
 				this.currently_played_effects.set(effect.id, effect)
 				this.managers.videoManager.add_video_to_canvas(effect)
-				const element = this.managers.videoManager.get(effect.id)?._originalElement as HTMLVideoElement
+				const element = this.managers.videoManager.get(effect.id)?.texture.source.resource as HTMLVideoElement
 				if(element) {element.currentTime = effect.start / 1000}
 			}
 			else if(effect.kind === "text") {
 				this.currently_played_effects.set(effect.id, effect)
-				this.managers.textManager.add_text_to_canvas(effect)
+				// this.managers.textManager.add_text_to_canvas(effect)
 			}
 			else if(effect.kind === "audio") {
 				this.currently_played_effects.set(effect.id, effect)
@@ -235,7 +258,7 @@ export class Compositor {
 			}
 			else if(effect.kind === "text") {
 				this.currently_played_effects.delete(effect.id)
-				this.managers.textManager.remove_text_from_canvas(effect)
+				// this.managers.textManager.remove_text_from_canvas(effect)
 			}
 			else if(effect.kind === "video") {
 				this.currently_played_effects.delete(effect.id)
@@ -254,52 +277,47 @@ export class Compositor {
 	}
 
 	init_guidelines() {
-		const guideline = new AlignGuidelines({
-			canvas: this.canvas,
-			aligningOptions: {
-				lineColor: "#03a9c1"
-			}
-		})
-		guideline.init()
+		if(this.guidelines)
+			this.guidelines.init()
 		// add rect as big as canvas so it acts as guideline for canvas borders
-		const rect = new Rect({width: 1920, height: 1080, fill: "transparent", selectable: false, evented: false, rect_type: "guideline"})
-		this.canvas.moveObjectTo(rect, 999)
-		this.canvas.add(rect)
+		// const rect = new Rect({width: 1920, height: 1080, fill: "transparent", selectable: false, evented: false, rect_type: "guideline"})
+		// this.canvas.moveObjectTo(rect, 999)
+		// this.canvas.add(rect)
 	}
 
 	#on_new_canvas_object_set_handle_styles() {
-		this.canvas.on("object:added", (e) => {
-			e.target.set({
-				transparentCorners: false,
-				borderColor: "#03a9c1",
-				cornerColor: "white"
-			})
-		})
+		// this.canvas.on("object:added", (e) => {
+		// 	e.target.set({
+		// 		transparentCorners: false,
+		// 		borderColor: "#03a9c1",
+		// 		cornerColor: "white"
+		// 	})
+		// })
 	}
 
 	#on_selected_canvas_object() {
-		this.canvas.on("mouse:down", (e) => {
+		this.app.stage.on("pointerdown", (e) => {
 			//@ts-ignore
 			const selected_effect = e.target ? e.target.effect as AnyEffect : null
 			this.actions.set_selected_effect(selected_effect, {omit: true})
-			if(e.target) {this.canvas.setActiveObject(e.target)}
-			if(selected_effect?.kind === "text") {this.managers.textManager.set_clicked_effect(selected_effect)}
+			// if(e.target) {this.canvas.setActiveObject(e.target)}
+			// if(selected_effect?.kind === "text") {this.managers.textManager.set_clicked_effect(selected_effect)}
 		})
-		this.canvas.on("mouse:up", (e) => {
+		this.app.stage.on("pointerup", (e) => {
 			//@ts-ignore
 			const selected_effect = e.target ? e.target.effect as Exclude<AnyEffect, AudioEffect> : null
 			if(selected_effect) {
 				const {rect: {position_on_canvas: {x, y}}} = selected_effect
-				if(x !== e.target!.left || y !== e.target!.top) {
-					this.actions.set_position_on_canvas(selected_effect, e.target!.left, e.target!.top);
-					this.actions.set_rotation(selected_effect, e.target!.angle)
-					this.actions.set_effect_scale(selected_effect, e.target!.getObjectScaling())
+				if(x !== e.target.x || y !== e.target.y) {
+					this.actions.set_position_on_canvas(selected_effect, e.target.x, e.target.y);
+					this.actions.set_rotation(selected_effect, e.target.angle)
+					this.actions.set_effect_scale(selected_effect, {x: e.target.scale.x, y: e.target.scale.y})
 					//@ts-ignore
 					e.target.effect = {...e.target.effect,
-						rect: {position_on_canvas: {x: e.target!.left, y: e.target!.top},
-						rotation: e.target!.angle,
-						scaleX: e.target!.getObjectScaling().x,
-						scaleY: e.target!.getObjectScaling().y}
+						rect: {position_on_canvas: {x: e.target.x, y: e.target.y},
+						rotation: e.target.angle,
+						scaleX: e.target.scale.x,
+						scaleY: e.target.scale.y}
 					} as Exclude<AnyEffect, AudioEffect>
 				}
 			}
@@ -326,48 +344,48 @@ export class Compositor {
 					this.managers.audioManager.add_audio_effect(effect, file, true)
 			}
 			else if(effect.kind === "text") {
-				this.managers.textManager.add_text_effect(effect, true)
+				// this.managers.textManager.add_text_effect(effect, true)
 			}
 		}
 		for(const filter of state.filters) {
 			const effect = state.effects.find(e => e.id === filter.targetEffectId)
 			if(effect && (effect.kind === "video" || effect.kind === "image")) {
-				this.managers.filtersManager.addFilterToEffect(effect, filter.type)
+				// this.managers.filtersManager.addFilterToEffect(effect, filter.type)
 			}
 		}
-		this.managers.transitionManager.refresh(state)
-		this.managers.animationManager.refresh(state)
+		// this.managers.transitionManager.refresh(state)
+		// this.managers.animationManager.refresh(state)
 		this.compose_effects(state.effects, this.timecode)
 	}
 
 	update_canvas_objects(state: State) {
-		this.canvas.getObjects().forEach(object => {
-			if(!(object instanceof Rect)) {
+		this.app.stage.children.forEach(object => {
+			if(!(object instanceof Rectangle)) {
 				//@ts-ignore
 				const object_effect = object.effect as Exclude<AnyEffect, AudioEffect>
 				const effect = state.effects.find(effect => effect.id === object_effect.id)! as Exclude<AnyEffect, AudioEffect>
 				if(effect) {
-					object.left = effect.rect.position_on_canvas.x
-					object.top = effect.rect.position_on_canvas.y
+					object.x = effect.rect.position_on_canvas.x
+					object.y = effect.rect.position_on_canvas.y
 					object.angle = effect.rect.rotation
-					object.scaleX = effect.rect.scaleX
-					object.scaleY = effect.rect.scaleY
-					object.setCoords()
-					this.canvas.renderAll()
+					object.scale.x = effect.rect.scaleX
+					object.scale.y = effect.rect.scaleY
+					// object.setCoords()
+					this.app.render()
 				}
 			}
 		})
 	}
 
 	set_canvas_resolution(width: number, height: number) {
-		this.canvas.setDimensions({width, height})
-		const guideline_rect = this.canvas.getObjects().find(object => {
-			//@ts-ignore
-			if(object.rect_type === "guideline")
-				return object
-		})
-		guideline_rect?.scaleToWidth(width - 1)
-		guideline_rect?.scaleToHeight(height - 1)
+		// this.canvas.setDimensions({width, height})
+		// const guideline_rect = this.canvas.getObjects().find(object => {
+		// 	//@ts-ignore
+		// 	if(object.rect_type === "guideline")
+		// 		return object
+		// })
+		// guideline_rect?.scaleToWidth(width - 1)
+		// guideline_rect?.scaleToHeight(height - 1)
 	}
 
 	set_timebase(value: number) {
@@ -377,22 +395,22 @@ export class Compositor {
 	setOrDiscardActiveObjectOnCanvas(selectedEffect: AnyEffect | null, state: State) {
 		if (!selectedEffect) {
 			// Discard any active object if no effect is selected
-			this.canvas.discardActiveObject()
+			// this.canvas.discardActiveObject()
 			return
 		}
 
 		const isEffectOnCanvas = get_effect_at_timestamp(selectedEffect, state.timecode)
 
 		if (isEffectOnCanvas) {
-			const object = this.canvas.getObjects().find((object: any) =>
-				(object?.effect as AnyEffect)?.id === selectedEffect.id
-			)
+		// 	const object = this.canvas.getObjects().find((object: any) =>
+		// 		(object?.effect as AnyEffect)?.id === selectedEffect.id
+		// 	)
 
-			if (object && object !== this.canvas.getActiveObject()) {
-				this.canvas.setActiveObject(object)
-			}
-		} else {
-			this.canvas.discardActiveObject()
+		// 	if (object && object !== this.canvas.getActiveObject()) {
+		// 		this.canvas.setActiveObject(object)
+		// 	}
+		// } else {
+		// 	this.canvas.discardActiveObject()
 		}
 	}
 
