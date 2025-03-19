070967747414cf15646c735442d6f4cca2d944b6
zenkyu <przemekgg2002@gmail.com>
2025-03-19 01:06:59 +0100

    chore: add new packages

--- s/index.html.ts
+++ s/index.html.ts
@@ -7,8 +7,20 @@ export default template(async basic => {
 		css: "index.css",
 		title: "omniclip",
 		head: html`
-			<script src="https://pixijs.download/dev/pixi.min.js"></script>
-			<script src="https://cdn.jsdelivr.net/npm/pixi-filters@6.1.0/dist/pixi-filters.min.js"></script>
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.0/cdn/themes/dark.css" />
+			<script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.0/cdn/shoelace.js" ></script>
+			<!-- adding pixijs here because pixijs/filters work this way
+				(because pixijs/filters does some global stuff work which
+				doesnt seem to work when installing pixi through package json),
+				but also adding pixijs to package json for types  -->
+			<script src="https://cdn.jsdelivr.net/npm/pixi.js@7.4.2/dist/pixi.min.js"></script>
+			<script src="https://cdn.jsdelivr.net/npm/pixi-filters@5.3.0/dist/browser/pixi-filters.min.js"></script>
+			<script src="https://cdn.jsdelivr.net/npm/@pixi/graphics-extras@7.1.4/dist/graphics-extras.min.js"></script>
+			<script src="https://cdn.jsdelivr.net/npm/@pixi-essentials/object-pool@1.0.1/dist/pixi-object-pool.js"></script>
+			<script src="https://cdn.jsdelivr.net/npm/@pixi-essentials/bounds@3.0.0/dist/bounds.js"></script>
+			<script src="https://cdn.jsdelivr.net/npm/@pixi-essentials/transformer@3.0.2/dist/transformer.js"></script>
+			<!-- <script src="https://cdn.jsdelivr.net/npm/pixi.js@8.8.1/dist/pixi.min.js"></script> -->
+			<!-- <script src="https://cdn.jsdelivr.net/npm/pixi-filters@6.1.0/dist/pixi-filters.min.js"></script> -->
 			<script src="coi-serviceworker.js"></script>
 			<meta charset="UTF-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
