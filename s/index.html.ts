209e5a2044aeef584d0fb9136d87686683a96c4c
Przemek <przemekgg2002@gmail.com>
2025-07-21 07:11:01 +0200

    add: shoelace and landing page component to index html

--- s/index.html.ts
+++ s/index.html.ts
@@ -5,7 +5,6 @@ import {socialCard} from "./website/social-card.js"
 
 export default ssg.page(import.meta.url, async orb => {
 	const title = "Omniclip"
-	const version = await orb.packageVersion()
 
 	return ({
 		title,
@@ -16,18 +15,11 @@ export default ssg.page(import.meta.url, async orb => {
 		socialCard: socialCard(title),
 		head: html`
 			<meta data-version="${await orb.packageVersion("$/package.json")}"/>
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.1/cdn/themes/dark.css">
+			<script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.1/cdn/shoelace.js"></script>
 		`,
 		body: html`
-			<section class=lead>
-				<div class=logobox>
-					<img class=logo alt="" src="/assets/logo/omni.avif"/>
-				</div>
-				<div class=contentbox>
-					<h1>Omniclip <span>v${version}</span></h1>
-					<p><a href="/editor/">Launch Editor</a></p>
-					<p><a href="https://github.com/omni-media/omniclip">github.com/omni-media/omniclip</a></p>
-				</div>
-			</section>
+			<landing-page></landing-page>
 		`,
 	})
 })
--- s/main.bundle.ts
+++ s/main.bundle.ts
@@ -1,3 +1,6 @@
+import {register} from "@benev/slate"
+import {landingPage} from "./website/index.js"
 
 console.log("🌀 Omniclip")
 
+register({landingPage})
