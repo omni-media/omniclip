7f9f820dca440bd720182be44ae4ca449decd349
Przemek Gałęzki <przemekgg2002@gmail.com>
2026-03-25 22:00:53 +0100

    remove: obsolete code

--- s/editor/ui/pages/project/tabbing/tabs/edit/views/items/stack/view.ts
+++ s/editor/ui/pages/project/tabbing/tabs/edit/views/items/stack/view.ts
@@ -1,67 +1,67 @@
-import {html} from "lit"
-import {view} from "@e280/sly"
-import {Item, Kind} from "@omnimedia/omnitool"
-import {ms} from "@omnimedia/omnitool/x/units/ms.js"
-
-import styleCss from "./style.css.js"
-import {renderItem} from "../../../parts/render-item.js"
-import themeCss from "../../../../../../../../../theme.css.js"
-import {EditorContext} from "../../../../../../../../../context/context.js"
-
-export const StackView = view(use => (
-	context: EditorContext,
-	item: Item.Stack,
-	ancestors: Item.Any[]
-) => {
-	use.styles(themeCss, styleCss)
-	const session = context.session
-	const timeline = context.strata.timeline.state
-
-	const getStackDuration = (item: Item.Stack, items: Item.Any[]) => {
-		return Math.max(0, ...item.childrenIds.map(id => {
-			const child = items.find(item => item.id === id)
-			if(child?.kind === Kind.Video)
-				return child?.duration
-			else return 0
-		}))
-	}
-
-	const setViewedItem = async () => session.$viewedItemId.value = item.id
-
-	const renderFullMode = () => {
-		return html`
-			<div class="stack-tracks">
-			${item.childrenIds.map(childId => html`
-				<div class=stack-track>
-					${renderItem(context, childId, [...ancestors, item])}
-				</div>
-			`)}
-			</div>
-		`
-	}
-
-	const renderCompactMode = () => {
-		const stackDuration = getStackDuration(item, timeline.items as Item.Any[])
-		const visualWidth = session.viewport.durationToWidth(ms(stackDuration))
-
-		return html`
-			<div
-				class="stack-block"
-				style="
-					height: 60px;
-					width: ${visualWidth}px;
-				"
-				@dblclick=${setViewedItem}
-				title="Explore Stack"
-			>
-				<span class="label">Stack (${item.id})</span>
-			</div>
-		`
-	}
-
-	if (ancestors.length === 0) {
-		return renderFullMode()
-	} else {
-		return renderCompactMode()
-	}
-})
+// import {html} from "lit"
+// import {view} from "@e280/sly"
+// import {Item, Kind} from "@omnimedia/omnitool"
+// import {ms} from "@omnimedia/omnitool/x/units/ms.js"
+//
+// import styleCss from "./style.css.js"
+// import {renderItem} from "../../../parts/render-item.js"
+// import themeCss from "../../../../../../../../../theme.css.js"
+// import {EditorContext} from "../../../../../../../../../context/context.js"
+//
+// export const StackView = view(use => (
+// 	context: EditorContext,
+// 	item: Item.Stack,
+// 	ancestors: Item.Any[]
+// ) => {
+// 	use.styles(themeCss, styleCss)
+// 	const session = context.session
+// 	const timeline = context.strata.timeline.state
+//
+// 	const getStackDuration = (item: Item.Stack, items: Item.Any[]) => {
+// 		return Math.max(0, ...item.childrenIds.map(id => {
+// 			const child = items.find(item => item.id === id)
+// 			if(child?.kind === Kind.Video)
+// 				return child?.duration
+// 			else return 0
+// 		}))
+// 	}
+//
+// 	const setViewedItem = async () => session.$viewedItemId.value = item.id
+//
+// 	const renderFullMode = () => {
+// 		return html`
+// 			<div class="stack-tracks">
+// 			${item.childrenIds.map(childId => html`
+// 				<div class=stack-track>
+// 					${renderItem(context, childId, [...ancestors, item])}
+// 				</div>
+// 			`)}
+// 			</div>
+// 		`
+// 	}
+//
+// 	const renderCompactMode = () => {
+// 		const stackDuration = getStackDuration(item, timeline.items as Item.Any[])
+// 		const visualWidth = session.viewport.durationToWidth(ms(stackDuration))
+//
+// 		return html`
+// 			<div
+// 				class="stack-block"
+// 				style="
+// 					height: 60px;
+// 					width: ${visualWidth}px;
+// 				"
+// 				@dblclick=${setViewedItem}
+// 				title="Explore Stack"
+// 			>
+// 				<span class="label">Stack (${item.id})</span>
+// 			</div>
+// 		`
+// 	}
+//
+// 	if (ancestors.length === 0) {
+// 		return renderFullMode()
+// 	} else {
+// 		return renderCompactMode()
+// 	}
+// })
--- s/editor/ui/pages/project/tabbing/tabs/edit/views/items/video/style.css.ts
+++
@@ -1,7 +0,0 @@
-import {css} from "lit"
-
-export default css`@layer view {
-:host {
-	height: 100%;
-}
-}`
